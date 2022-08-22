4b668cd82226ab00464053aeec9a88660807834f
Ravana <47528708+ravana69@users.noreply.github.com>
2022-08-22 12:08:45 +0530

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+import { ShaderArt } from 'https://cdn.skypack.dev/shader-art';
+import { UniformPlugin } from 'https://cdn.skypack.dev/@shader-art/plugin-uniform';
+
+const uniforms = () => new UniformPlugin(); // not needed for now.
+
+ShaderArt.register([]);
